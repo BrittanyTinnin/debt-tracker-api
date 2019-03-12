7c492b2756e56a02222424bd881d4141775a28c5
github username <github email address>
2019-03-12 16:58:44 -0400

    create crud actions

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,2 +1,20 @@
 class BillsController < ApplicationController
+
+    def index
+        @bills = Bill.all
+    end
+
+    def show
+        @bill = Bill.find_by(id: params[:id])
+    end
+
+    def create
+        bill = Bill.new(bill_params)
+    end
+
+    private
+        def bill_params
+            
+        end
+
 end
